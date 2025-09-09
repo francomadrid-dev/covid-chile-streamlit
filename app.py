a38fa78933159f04661fcef954bbecb954a35814
francomadrid-dev <f.madridn@gmail.com>
2025-09-08 23:52:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
 from datetime import datetime, timedelta
-
+st.sidebar.info(f"API key cargada: {bool(API_KEY)} — largo: {len(API_KEY)}")
 st.set_page_config(page_title="Calidad del Aire Chile (OpenAQ)", layout="wide")
 
 # ===== Configuración =====
